b30c87edcee7a38fb3afad9e3063c21c1b113fcd
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-04 14:22:07 +0100

    add findbyId and create routes

--- server/api/controllers/items.js
+++ server/api/controllers/items.js
@@ -16,4 +16,26 @@ router.get('/', async (req,res) => {
 })
 
 
+router.get('/:id', async (req,res) => {
+    try{
+        const item = await Item.findById(parseInt(req.params.id))
+        res.json(item)
+    }
+
+    catch(err){
+        res.status(500).json({err})
+}
+
+})
+
+
+router.post('/', async (req, res) => {
+    try {
+        const newItem = await Item.create(req.body.item_name, req.body.amount)
+        res.json(newItem)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+})
+
 module.exports = router;
--- server/api/models/items.js
+++ server/api/models/items.js
@@ -9,6 +9,7 @@ class Item {
     }
 
  static get all(){
+     //use get so we can call it without the () in controllers
     return new Promise (async (resolve, reject) => {
         try {
             const itemsData = await db.query(`SELECT * FROM items;`)
@@ -21,7 +22,35 @@ class Item {
         }
     })
 
+}
+
+    static findById (id) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let itemsData = await db.query(`SELECT * FROM items WHERE id = $1;`, [ id ]);
+                let item = new Item(itemsData.rows[0]);
+                resolve (item);
+            } catch (err) {
+                reject('Item not found');
+            }
+        })
     }
+
+    static create(item_name, amount){
+        return new Promise (async (resolve, reject) => {
+            try {
+                let itemData = await db.query(`INSERT INTO items (item_name, amount) VALUES ($1, $2) RETURNING *;`, [ item_name, amount ]);
+                let newItem = new Item(itemData.rows[0]);
+                resolve (newItem);
+            } catch (err) {
+                reject('Error creating item');
+            }
+        });
+    }
+
+
+
+
 }
 
 
